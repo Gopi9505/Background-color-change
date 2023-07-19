fc18825bb145d4ca92c803491a17038a60ea889e
Mittakola Gopi <72293454+Gopi9505@users.noreply.github.com>
2023-07-19 19:32:16 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-function bgchange(id)
-{
-    document.body.style.background = document.getElementById(id).innerHTML;
+function changeBG(val){
+    console.log(val.innerHTML);
+    document.body.style.background = val.innerHTML;
+}
+
+function magic(){
+    var color = ['Red','Yellow','Brown','Blue','Pink','Green','Orange','Violet','Indigo'];
+    var button = document.getElementById("btn");
+    var i=0;
+    function fun(){
+        setTimeout(()=>{
+            document.body.style.background = color[i];
+            button.style.backgroundColor = color[i];
+            button.innerText = color[i];
+            i++;
+            if(i<=color.length){
+                fun();
+            }else{
+                button.innerText = 'click me';
+                button.style.backgroundColor = 'white';
+                document.body.style.background = 'darkgray';
+            }
+        },1000)
+    }
+    fun();
 }
